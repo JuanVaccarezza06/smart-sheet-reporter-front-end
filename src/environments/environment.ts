216c140f121fc0f525ca062f457732f8c5a8a9d7
Juan Pablo Vaccarezza <juanpirvaccarezza@gmail.com>
2026-03-04 23:27:17 -0300

    I setting the two api ruls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'smart-sheet-reporter-production.up.railway.app' // Aquí pondremos el dominio de Railway
+  // apiUrl: 'http://localhost:8080' // Aquí pondremos el dominio de Railway
 };
