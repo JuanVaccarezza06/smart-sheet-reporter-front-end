bffde34436113323dc25ef121d019a22ca23ccba
Juan Pablo Vaccarezza <juanpirvaccarezza@gmail.com>
2026-03-04 23:17:55 -0300

    im using de environment api rul variable

--- src/app/services/document-generator-service.ts
+++ src/app/services/document-generator-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +9,15 @@ import { Observable } from 'rxjs';
 export class DocumentGeneratorService {
   private http = inject(HttpClient);
 
-  private readonly API_URL =
-    'smart-sheet-reporter-production.up.railway.app/smart-sheet-reporter/api/beta/generator/generate';
+  private readonly API_URL = environment.apiUrl;
+  private readonly endpoint = 'smart-sheet-reporter/api/beta/generator/generate';
 
   generateDocuments(excel: File, template: File): Observable<Blob> {
     const formData = new FormData();
     formData.append('excel', excel);
     formData.append('template', template);
 
-    return this.http.post(this.API_URL, formData, {
+    return this.http.post(`${this.API_URL}/${this.endpoint}`, formData, {
       responseType: 'blob',
     });
   }
